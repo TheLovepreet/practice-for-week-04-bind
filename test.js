afc5f746810fa6c49f540197ce7fcdfd7e0cf1b0
TheLove <lovepreetsinghcad@gmail.com>
2022-11-23 18:53:16 -0500

    Final Commit

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
 // Your code here
+const Employee = require("./employee");
+
+const John = new Employee("John Wick", "Dog Lover");
+
+const unboudned = John.sayName;
+const bounded = unboudned.bind(John);
+
+setTimeout(bounded,2000);
+
+const unboudnedOcc = John.sayOccupation;
+const boundedOcc = unboudnedOcc.bind(John);
+
+setTimeout(boundedOcc,3000);
